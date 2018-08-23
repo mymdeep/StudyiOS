62d5df961976f55c0e61c0516de222253138a6e5
王飞 <wangfei@ali-6c96cfdfd171.local>
2018-08-23 09:14:17 +0800

    Initial Commit

---
+++ MyDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDemo
+//
+//  Created by 王飞 on 2018/8/23.
+//  Copyright © 2018年 王飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDemo
+//
+//  Created by 王飞 on 2018/8/23.
+//  Copyright © 2018年 王飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
